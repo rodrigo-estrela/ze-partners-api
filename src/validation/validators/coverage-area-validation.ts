1698adf9958baa9016de07bd1b03289f417cf2bd
Rodrigo Estrela <dev.rodrigoestrela@gmail.com>
2021-02-05 16:51:03 -0300

    refactor: fix typo at coverageAreaValidator name

--- src/validation/validators/coverage-area-validation.ts
+++ src/validation/validators/coverage-area-validation.ts
@@ -2,10 +2,10 @@ import { Validation } from '@/presentation/protocols'
 import { CoverageAreaValidator } from '@/validation/protocols'
 
 export class CoverageAreaValidation implements Validation {
-  constructor (private readonly fieldName: string, private readonly coveraAreaValidator: CoverageAreaValidator) { }
+  constructor (private readonly fieldName: string, private readonly coverageAreaValidator: CoverageAreaValidator) { }
 
   validate (input: any): Error {
-    const isValid = this.coveraAreaValidator.isValid(input)
+    const isValid = this.coverageAreaValidator.isValid(input)
     if (!isValid) {
       return new Error(`Invalid Param: ${this.fieldName}`)
     }
