976d5abdaac976b6f5585e148b68e2c32c84d69f
Rodrigo Estrela <dev.rodrigoestrela@gmail.com>
2021-02-05 16:45:11 -0300

    feat: ensure CoverageAreaValidation calls CoverageAreaValidator with correct data

--- tests/validation/validators/coverage-area-validation.spec.ts
+++ tests/validation/validators/coverage-area-validation.spec.ts
@@ -25,4 +25,11 @@ describe('CoverageAreaValidation', () => {
     const error = sut.validate({ [fieldName]: 'valid_coverage_area' })
     expect(error).toEqual(new Error(`Invalid Param: ${fieldName}`))
   })
+
+  it('Should all CoverageAreaValidator with correct data', () => {
+    const { sut, coverageAreaValidatorSpy } = makeSut()
+    const isValidSpy = jest.spyOn(coverageAreaValidatorSpy, 'isValid')
+    sut.validate('valid_coverage_area')
+    expect(isValidSpy).toHaveBeenCalledWith('valid_coverage_area')
+  })
 })
