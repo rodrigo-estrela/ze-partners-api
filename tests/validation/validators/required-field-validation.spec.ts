38a86b43600f064b0d9b4a0318113991a4db54d2
Rodrigo Estrela <dev.rodrigoestrela@gmail.com>
2021-02-06 03:09:15 -0300

    refactor: move missing param error to a hepler method

--- tests/validation/validators/required-field-validation.spec.ts
+++ tests/validation/validators/required-field-validation.spec.ts
@@ -1,7 +1,8 @@
 import { RequiredFieldValidation } from '@/validation/validators'
+import { MissingParamError } from '@/presentation/errors'
 
 const makeSut = (): RequiredFieldValidation => {
-  const sut = new RequiredFieldValidation('validField')
+  const sut = new RequiredFieldValidation('valid_field')
 
   return sut
 }
@@ -10,12 +11,12 @@ describe('RequiredFieldValidation', () => {
   it('Should return a MissingParamError if validation fails', () => {
     const sut = makeSut()
     const error = sut.validate({ invalidField: 'any_value' })
-    expect(error).toEqual(new Error('Missing Param: validField'))
+    expect(error).toEqual(new MissingParamError('valid_field'))
   })
 
   it('Should not return if validation succeeds', () => {
     const sut = makeSut()
-    const error = sut.validate({ validField: 'any_value' })
+    const error = sut.validate({ valid_field: 'any_value' })
     expect(error).toBeFalsy()
   })
 })
