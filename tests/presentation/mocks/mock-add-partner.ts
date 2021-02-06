9031ca886c8c1c185d0853b10dcdb85490880c86
Rodrigo Estrela <dev.rodrigoestrela@gmail.com>
2021-02-06 04:27:13 -0300

    refactor: tranform AddPartnerSpy add method in async

--- tests/presentation/mocks/mock-add-partner.ts
+++ tests/presentation/mocks/mock-add-partner.ts
@@ -4,7 +4,7 @@ import { PartnerModel } from '@/domain/models'
 export class AddPartnerSpy implements AddPartner {
   params: AddPartnerParams
 
-  add (params: AddPartnerParams): PartnerModel {
+  async add (params: AddPartnerParams): Promise<PartnerModel> {
     this.params = params
     return Object.assign({}, { id: 'any_id' }, params)
   }
