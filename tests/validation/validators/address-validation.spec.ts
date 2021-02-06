902761c630d098a48c38fb9b3adfb056d8f666cc
Rodrigo Estrela <dev.rodrigoestrela@gmail.com>
2021-02-05 23:32:11 -0300

    feat: ensure AddressValidation calls AddressValidator with correct data

--- tests/validation/validators/address-validation.spec.ts
+++ tests/validation/validators/address-validation.spec.ts
@@ -19,10 +19,17 @@ const makeSut = (): SutTypes => {
 }
 
 describe('AddressValidation', () => {
-  it('Should return an error if CoverageAreaValidator returns false', () => {
+  it('Should return an error if AddressValidator returns false', () => {
     const { sut, addressValidatorSpy } = makeSut()
     addressValidatorSpy.isAddressValid = false
     const error = sut.validate({ [fieldName]: 'valid_address' })
     expect(error).toEqual(new Error(`Invalid Param: ${fieldName}`))
+  })
+
+  it('Should call AddressValidator with correct data', () => {
+    const { sut, addressValidatorSpy } = makeSut()
+    const isValidSpy = jest.spyOn(addressValidatorSpy, 'isValid')
+    sut.validate('valid_coverage_area')
+    expect(isValidSpy).toHaveBeenCalledWith('valid_coverage_area')
   })
 })
