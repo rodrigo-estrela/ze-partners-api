f9e5628285a392bab071408426971f0e13a59532
Rodrigo Estrela <dev.rodrigoestrela@gmail.com>
2021-02-07 01:20:08 -0300

    feat: ensure request body is parsed as json

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,6 +1,4 @@
-import express from 'express'
-
-const app = express()
+import { app } from './config'
 
 app.listen(1010, () => {
   console.log('Server is running at http://localhost:1010')
---
+++ tests/main/middlewares/body-parser.test.ts
@@ -0,0 +1,14 @@
+import request from 'supertest'
+import { app } from '@/main/config'
+
+describe('Body Parser Middleware', () => {
+  it('Should parse body as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      res.send(req.body)
+    })
+    await request(app)
+      .post('/test_body_parser')
+      .send({ field: 'hire me please!' })
+      .expect({ field: 'hire me please!' })
+  })
+})
