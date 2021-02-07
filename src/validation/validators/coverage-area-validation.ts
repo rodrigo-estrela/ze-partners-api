bf9f7897db3270f565150523cd432f6a58bb169e
Rodrigo Estrela <dev.rodrigoestrela@gmail.com>
2021-02-07 08:08:36 -0300

    refactor: correct the value that Validation calls Validator

--- src/validation/validators/coverage-area-validation.ts
+++ src/validation/validators/coverage-area-validation.ts
@@ -1,12 +1,13 @@
 import { Validation } from '@/presentation/protocols'
 import { CoverageAreaValidator } from '@/validation/protocols'
-import { InvalidParamError } from '@/presentation/errors'
+import { InvalidParamError } from '../../presentation/errors'
 
 export class CoverageAreaValidation implements Validation {
   constructor (private readonly fieldName: string, private readonly coverageAreaValidator: CoverageAreaValidator) { }
 
   validate (input: any): Error {
-    const isValid = this.coverageAreaValidator.isValid(input)
+    console.log('coverageArea', input)
+    const isValid = this.coverageAreaValidator.isValid(input[this.fieldName])
     if (!isValid) {
       return new InvalidParamError(this.fieldName)
     }
