0fda10ef69f39c0b4b13e9f47ea6048f0d0bf3bd
Rodrigo Estrela <dev.rodrigoestrela@gmail.com>
2021-02-08 00:12:20 -0300

    refactor: ensure handle parameter from LoadPartnerController conforms to a type

--- src/presentation/controllers/load-partner-controller.ts
+++ src/presentation/controllers/load-partner-controller.ts
@@ -1,12 +1,18 @@
 import { CheckPartnerById } from '@/domain/usecases'
-import { HttpRequest, HttpResponse } from '../helpers'
+import { HttpResponse } from '../helpers'
 import { Controller } from '../protocols'
 
 export class LoadPartnerController implements Controller {
   constructor (private readonly checkPartnerById: CheckPartnerById) { }
 
-  async handle (request: HttpRequest): Promise<HttpResponse> {
-    await this.checkPartnerById.checkById(request.body.id)
+  async handle (request: LoadPartnerController.Request): Promise<HttpResponse> {
+    await this.checkPartnerById.checkById(request.partnerId)
     return null
+  }
+}
+
+export namespace LoadPartnerController {
+  export type Request = {
+    partnerId: string
   }
 }
--- tests/presentation/controllers/load-partner-controller.spec.ts
+++ tests/presentation/controllers/load-partner-controller.spec.ts
@@ -1,10 +1,8 @@
 import { CheckPartnerByIdSpy } from '../mocks'
 import { LoadPartnerController } from '@/presentation/controllers'
 
-const request = {
-  body: {
-    id: 'valid_id'
-  }
+const request: LoadPartnerController.Request = {
+  partnerId: 'valid_id'
 }
 
 type SutTypes = {
@@ -26,6 +24,6 @@ describe('LoadPartner Controller', () => {
   it('Should call CheckPartnerById with correct value', async () => {
     const { sut, checkPartnerByIdSpy } = makeSut()
     await sut.handle(request)
-    expect(checkPartnerByIdSpy.partnerId).toBe(request.body.id)
+    expect(checkPartnerByIdSpy.partnerId).toBe(request.partnerId)
   })
 })
