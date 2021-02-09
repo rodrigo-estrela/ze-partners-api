2d04cfa0064b78d3fe5e4f362353aae9858df17b
Rodrigo Estrela <dev.rodrigoestrela@gmail.com>
2021-02-09 01:01:46 -0300

    feat: ensure SearchPartnerController call SearchNearestPartner with correct values

--- src/presentation/controllers/search-partner-controller.ts
+++ src/presentation/controllers/search-partner-controller.ts
@@ -1,12 +1,15 @@
+import { SearchNearestPartner } from '@/domain/usecases'
 import { badRequest, HttpResponse } from '../helpers'
 import { Controller, Validation } from '../protocols'
 
 export class SearchPartnerController implements Controller {
-  constructor (private readonly validation: Validation) { }
+  constructor (private readonly validation: Validation, private readonly searchNearestPartner: SearchNearestPartner) { }
 
   async handle (request: any): Promise<HttpResponse> {
     const error = this.validation.validate(request)
     if (error) return badRequest(error)
+
+    const partner = await this.searchNearestPartner.search(request)
     return null
   }
 }
--- tests/presentation/controllers/search-partner-controller.spec.ts
+++ tests/presentation/controllers/search-partner-controller.spec.ts
@@ -1,21 +1,24 @@
 import { SearchPartnerController } from '@/presentation/controllers'
 import { badRequest } from '@/presentation/helpers'
-import { ValidationSpy } from '../mocks'
+import { ValidationSpy, SearchNearestPartnerSpy } from '../mocks'
 
 const request: SearchPartnerController.Request = { lon: 'any_long', lat: 'any_lat' }
 
 type SutTypes = {
   sut: SearchPartnerController
   validationSpy: ValidationSpy
+  searchNearestPartnerSpy: SearchNearestPartnerSpy
 }
 
 const makeSut = (): SutTypes => {
   const validationSpy = new ValidationSpy()
-  const sut = new SearchPartnerController(validationSpy)
+  const searchNearestPartnerSpy = new SearchNearestPartnerSpy()
+  const sut = new SearchPartnerController(validationSpy, searchNearestPartnerSpy)
 
   return {
     sut,
-    validationSpy
+    validationSpy,
+    searchNearestPartnerSpy
   }
 }
 
@@ -31,5 +34,11 @@ describe('SearchPartner Controller', () => {
     validationSpy.error = new Error()
     const httpResponse = await sut.handle(request)
     expect(httpResponse).toEqual(badRequest(validationSpy.error))
+  })
+
+  it('Should call SearchNearestPartner with correct values', async () => {
+    const { sut, searchNearestPartnerSpy } = makeSut()
+    await sut.handle(request)
+    expect(searchNearestPartnerSpy.params).toEqual(request)
   })
 })
