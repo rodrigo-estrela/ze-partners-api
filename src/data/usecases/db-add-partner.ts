931ad195b39bb4968df7eee9fbf8cc0d6c487126
Rodrigo Estrela <dev.rodrigoestrela@gmail.com>
2021-02-09 23:55:59 -0300

    refactor: move partnerParams setup to a helper method

--- src/data/usecases/db-add-partner.ts
+++ src/data/usecases/db-add-partner.ts
@@ -1,11 +1,11 @@
 import { PartnerModel } from '@/domain/models'
-import { AddPartner, AddPartnerParams } from '@/domain/usecases'
+import { AddPartner } from '@/domain/usecases'
 import { AddPartnerRepository, CheckPartnerByDocumentRepository } from '../protocols/db/partner'
 
 export class DbAddPartner implements AddPartner {
   constructor (private readonly addPartnerRepository: AddPartnerRepository, private readonly checkPartnerByDocumentRepository: CheckPartnerByDocumentRepository) { }
 
-  async add (data: AddPartnerParams): Promise<PartnerModel> | undefined {
+  async add (data: AddPartner.Params): Promise<PartnerModel> | undefined {
     const exists = await this.checkPartnerByDocumentRepository.checkByDocument(data.document)
     if (exists) return undefined
 
--- tests/infra/db/mongodb/partner-mongo-repository.spec.ts
+++ tests/infra/db/mongodb/partner-mongo-repository.spec.ts
@@ -1,28 +1,13 @@
 import { MongoHelper, PartnerMongoRepository } from '@/infra/db/mongodb'
-import { AddPartnerParams } from '@/domain/usecases'
 
 import { Collection } from 'mongodb'
 
 import FakeObjectId from 'bson-objectid'
+import { mockAddPartnerParams } from '@/tests/domain/mock-partner-model'
 
 let partnerCollection: Collection
 
-const addPartnerParams: AddPartnerParams = {
-  tradingName: 'Adega da Cerveja - Pinheiros',
-  ownerName: 'Zé da Silva',
-  document: '1432132123891/0001',
-  coverageArea: {
-    type: 'MultiPolygon',
-    coordinates: [
-      [[[30, 20], [45, 40], [10, 40], [30, 20]]],
-      [[[15, 5], [40, 10], [10, 20], [5, 10], [15, 5]]]
-    ]
-  },
-  address: {
-    type: 'Point',
-    coordinates: [-46.57421, -21.785741]
-  }
-}
+const addPartnerParams = mockAddPartnerParams()[0]
 
 const makeSut = (): PartnerMongoRepository => {
   return new PartnerMongoRepository()
@@ -76,38 +61,7 @@ describe('PartnerMongoRepositoy', () => {
 
   describe('search()', () => {
     it('Should return null if none of the partners coverageArea intersects with provided location', async () => {
-      const partners = [
-        {
-          tradingName: 'square_0',
-          ownerName: 'owner_name_0',
-          document: 'document_0',
-          coverageArea: {
-            type: 'MultiPolygon',
-            coordinates: [
-              [[[0, 0], [0, 4], [4, 4], [4, 0], [0, 0]]]
-            ]
-          },
-          address: {
-            type: 'Point',
-            coordinates: [0, 0]
-          }
-        },
-        {
-          tradingName: 'square_1',
-          ownerName: 'owner_name_1',
-          document: 'document_1',
-          coverageArea: {
-            type: 'MultiPolygon',
-            coordinates: [
-              [[[0, 5], [0, 9], [4, 9], [4, 5], [0, 5]]]
-            ]
-          },
-          address: {
-            type: 'Point',
-            coordinates: [0, 5]
-          }
-        }
-      ]
+      const partners = mockAddPartnerParams()
       await partnerCollection.insertMany(partners)
       const sut = makeSut()
       const location = { lon: 2, lat: 10 }
@@ -116,38 +70,7 @@ describe('PartnerMongoRepositoy', () => {
     })
 
     it('Should return correct partner', async () => {
-      const partners = [
-        {
-          tradingName: 'square_0',
-          ownerName: 'owner_name_0',
-          document: 'document_0',
-          coverageArea: {
-            type: 'MultiPolygon',
-            coordinates: [
-              [[[0, 0], [0, 4], [4, 4], [4, 0], [0, 0]]]
-            ]
-          },
-          address: {
-            type: 'Point',
-            coordinates: [0, 0]
-          }
-        },
-        {
-          tradingName: 'square_1',
-          ownerName: 'owner_name_1',
-          document: 'document_1',
-          coverageArea: {
-            type: 'MultiPolygon',
-            coordinates: [
-              [[[0, 5], [0, 9], [4, 9], [4, 5], [0, 5]]]
-            ]
-          },
-          address: {
-            type: 'Point',
-            coordinates: [0, 5]
-          }
-        }
-      ]
+      const partners = mockAddPartnerParams()
       await partnerCollection.insertMany(partners)
       const sut = makeSut()
       const location = { lon: 2, lat: 2 }
